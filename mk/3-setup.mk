e3b544ba9a96febb689ca83854d7cc74ced5d952
benizar <benizar@gmail.com>
2018-05-10 16:55:01 +0200

    pulling geohashes of different precisions

--- mk/3-setup.mk
+++ mk/3-setup.mk
@@ -23,47 +23,53 @@ $(dirs):
 	mkdir -p $@
 
 
-# TODO: See the example
-#gh2 := xx
-#gh3 := xxx
-#gh4 := xxxx
-#gh5 := xxxxx
-#gh6 := xxxxxx
-#gh7 := xxxxxxx
-#geohashes := $(gh2) $(gh3) $(gh4) $(gh5) $(gh6)
-
-
 ###########
 # TARGETS #
 ###########
 # TODO: Ensure that these files exist or can be created from docker
-gh_list := $(out_dir)/gh-list.csv
+# TODO: Convert to define?
+gh2_csv := $(out_dir)/gh2.csv
+gh3_csv := $(out_dir)/gh3.csv
+gh4_csv := $(out_dir)/gh4.csv
+gh5_csv := $(out_dir)/gh5.csv
+gh6_csv := $(out_dir)/gh6.csv
 
-gh_list_targets += $(gh_list)
-pull_targets += $(gh_list)
+gh_csv_targets += $(gh2_csv) $(gh3_csv) $(gh4_csv) $(gh5_csv) $(gh6_csv)
+
+pull_targets += $(gh_csv_targets)
 
 #########
 # RULES #
 #########
 # TODO: This could be done by default (inmediately after 'make', not in a rule)
 ## Pull a list of geohashes from a geohashed docker database.
-pull-list: $(gh_list)
+pull-lists: $(gh_csv_targets)
 
-$(gh_list): | checkdirs
-	@echo -n 'Pulling a list of geohashes...'
-	@$(GET_CSV) /$@ $(FROM_SIOSE_2005) $(AS) "SELECT id FROM gh"
-	@echo "Done."
 
 # Get a list of geohashes and remove the column name.
-# This only works if gh-list already exists.
-geohashes := $(shell cat $(gh_list))
-geohashes := $(filter-out $(word 1, $(geohashes)),$(geohashes))
+# This only works if gh%_csv already exists.
+gh2 := $(shell cat $(gh2_csv))
+gh2 := $(filter-out $(word 1, $(gh2)),$(gh2))
 
+gh3 := $(shell cat $(gh3_csv))
+gh3 := $(filter-out $(word 1, $(gh3)),$(gh3))
+
+gh4 := $(shell cat $(gh4_csv))
+gh4 := $(filter-out $(word 1, $(gh4)),$(gh4))
+
+gh5 := $(shell cat $(gh5_csv))
+gh5 := $(filter-out $(word 1, $(gh5)),$(gh5))
+
+gh6 := $(shell cat $(gh6_csv))
+gh6 := $(filter-out $(word 1, $(gh6)),$(gh6))
+
+geohashes := $(gh2) $(gh3) $(gh4) $(gh5) $(gh6)
 
 #################
 # PATTERN RULES #
 #################
-
-
-
+$(out_dir)/gh%.csv: | checkdirs
+	@echo -n "Pulling list of geohashes (precision '$(*F)')..."
+	@$(GET_CSV) /$@ $(FROM_SIOSE_2005) $(AS) "SELECT id FROM gh WHERE precision='$(*F)'"
+	@echo "Done."
 
